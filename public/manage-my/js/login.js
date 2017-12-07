7a2a2af78e60189d690424dc4efd7b45bc3b7d2e
qy <qy>
2017-12-07 14:59:38 +0800

    测试验证功能

---
+++ public/manage-my/js/login.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('button[type=submit]').click(function(e){
+        e.preventDefault();
+        $.ajax({
+            url:'/employee/employeeLogin',
+            data:$('form').serialize(),
+            type:'post',
+            success:function(data){
+                console.log(data)
+            }   
+        })
+    })
+})
