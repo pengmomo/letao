ba22bd76f8c0aae38e01c18137f9b1311d14f382
qy <qy>
2017-12-09 12:05:24 +0800

    调用登出接口，判断登录状态

--- public/manage-my/js/index.js
+++ public/manage-my/js/index.js
@@ -1,4 +1,5 @@
 $(function () {
+
     var myChart1 = echarts.init(document.getElementById('main-left'));
     // 指定图表的配置项和数据
     var option1 = {
@@ -76,19 +77,36 @@ $(function () {
     myChart2.setOption(option2);
 
     // 点击收起左边
-    $('.lt-main .header a.glyphicon-align-justify').click(function(){
+    $('.lt-main .header a.glyphicon-align-justify').click(function () {
         $('.lt-aside').toggle();
         $('.lt-main').toggleClass('fullScreen');
     })
 
     // 点击弹出模态框
-    $('.lt-main .header a.glyphicon-log-out').click(function(){
+    $('.lt-main .header a.glyphicon-log-out').click(function () {
         $('.modal-sure').modal('show')
     })
 
-    // 点击确定退出系统调回登陆页面
-    $('.modal-sure button.btn-primary').click(function(){
+    // 点击确定退出系统调回登陆页面 调出登出接口
+    $('.modal-sure button.btn-primary').click(function () {
         $('.modal-sure').modal('hide');
-        window.location.href='./login.html'
+        $.ajax({
+            url: '/employee/employeeLogout',
+            success: function (data) {
+                console.log(data);
+                window.location.href = './login.html'
+            }
+        })
     })
+        // 判断是否登录
+    $.ajax({
+        url: '/employee/checkRootLogin',
+        success: function (data) {
+            if (data.error) {
+                window.location.href = './login.html'
+            }
+        }
+    })
+
+
 })
