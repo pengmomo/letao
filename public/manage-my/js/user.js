0f74ce12c305427c217bfa2404bacc399cbaef15
qy <qy>
2017-12-09 16:43:37 +0800

    分页插件的使用

--- public/manage-my/js/user.js
+++ public/manage-my/js/user.js
@@ -1,13 +1,33 @@
-$(function(){
-    $.ajax({
-        url:'/user/queryUser',
-        data:{
-            page:1,
-            pageSize:5,
-        },
-        success:function(data){
-            console.log(data);
-            $('tbody').html(template('userTmp',data));
-        }
-    })
+$(function () {
+
+    var mypageNum = 1;
+    var mypageSize = 5;
+
+    function getData() {
+        $.ajax({
+            url: '/user/queryUser',
+            data: {
+                page: mypageNum,
+                pageSize: mypageSize,
+            },
+            success: function (data) {
+                console.log(data);
+                $('tbody').html(template('userTmp', data));
+                $("#pagintor").bootstrapPaginator({
+                    bootstrapMajorVersion: 3, //默认是2，如果是bootstrap3版本，这个参数必填
+                    currentPage: mypageNum, //当前页
+                    totalPages: Math.ceil(data.total / data.size), //总页数
+                    size: "small", //设置控件的大小，mini, small, normal,large
+                    onPageClicked: function (event, originalEvent, type, page) {
+                        //为按钮绑定点击事件 page:当前点击的按钮值
+                        mypageNum = page;
+                        getData();
+                    }
+                });
+            }
+        })
+    }
+
+    getData()
+
 })
